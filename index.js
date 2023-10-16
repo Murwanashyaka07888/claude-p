514678fe3765a690a67160723837f98551c17418
Mahama Coding School <mahacodingclass@gmail.com>
2023-10-16 18:16:23 +0300

    new code adding

--- index.js
+++ index.js
@@ -7,5 +7,21 @@ $(document).ready(function(){
     $(window).on('scroll load',function(){
         $('#menu').removeclass('fa-times');
         $('header').removeclass('toggle'); 
-    })
+        if($(window).scrollTop() > 0){
+            $('.top').show();
+        }else{
+            $('.top').hiden();
+        }
+    });
+
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+        $('html,body').animate({
+        scrollTop:$($(this).attr('href')).offset().top,    
+        },
+        500,
+        'linear'
+        );
+
+    });
 });
